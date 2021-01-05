27ecbe6f930ee225e2b7000fe297f241d8ac1455
Rojanie <rojaniebeberino0206@gmail.com>
2021-01-06 01:42:56 +0800

    Kuya na stuck ako ulit...

--- action.php
+++ action.php
@@ -4,13 +4,25 @@
     if(isset($_POST["action"])){
 
         if($_POST['action'] == 'insert'){
-            $query = "
-                INSERT INTO forms (name, email, hobby) VALUES ('".$_POST['name']."', '".$_POST['email']."', '".$_POST['hobby']."')
-            ";
+            $query = "INSERT INTO forms (name, email) VALUES ('".$_POST['name']."', '".$_POST['email']."') ";
 
             $statement = $con->prepare($query);
             $statement->execute();
             echo '<p>Data Inserted.</p>';
+        }
+
+
+
+        if($_POST['action'] == 'fetch_single') {
+            $query = "SELECT * FROM forms WHERE id = '".$_POST['id']."' ";
+            $statement = $con->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            foreach($result as $row){
+                $output['name'] = $row['name'];    
+                $output['email'] = $row['eamil'];
+            }
+            echo json_encode($output);
         }
 
     }
